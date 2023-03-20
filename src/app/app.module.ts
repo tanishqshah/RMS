817b8c1049fe120cedc72b4835570f8f30441244
Karan Bengani <karanbengani@L-LTLF2NVJ4T.local>
2023-03-20 22:45:07 +0530

    app module errors resolved

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { AdminPanelComponent } from './admin-panel/admin-panel.component';
 import { ProjectManagerPanelComponent } from './project-manager-panel/project-manager-panel.component';
 import { NgxPaginationModule } from 'ngx-pagination';
+import { ManagerNavComponent } from './manager-nav/manager-nav.component';
+import { ProjectListComponent } from './project-list/project-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AdminPanelComponent,
     ProjectManagerPanelComponent,
+    ManagerNavComponent,
+    ProjectListComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, NgxPaginationModule],
   providers: [],
