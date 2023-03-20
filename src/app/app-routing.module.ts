ecb5715ca32bdb54b7b695ab493e128f5bdcda87
Karan Bengani <karanbengani@L-LTLF2NVJ4T.local>
2023-03-20 23:00:36 +0530

    updated manager navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminPanelComponent } from './admin-panel/admin-panel.component';
+import { ProjectManagerPanelComponent } from './project-manager-panel/project-manager-panel.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', component: ProjectManagerPanelComponent },
+  { path: 'admin', component: AdminPanelComponent },
+  { path: 'manager', component: ProjectManagerPanelComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/manager-nav/manager-nav.component.ts
+++ src/app/manager-nav/manager-nav.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-manager-nav',
   templateUrl: './manager-nav.component.html',
-  styleUrls: ['./manager-nav.component.scss']
+  styleUrls: ['./manager-nav.component.scss'],
 })
 export class ManagerNavComponent {
-
+  count = '1';
+  change(val: any) {
+    this.count = val;
+  }
 }
